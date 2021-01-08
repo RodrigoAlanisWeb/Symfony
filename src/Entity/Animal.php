1590eea145636233b4a3bb2d6a48851921058ab8
Rodrigo Ubuntu <Arkpex2007ps4@gmail.com>
2021-01-08 00:54:26 -0600

    Generate Migrations

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -4,15 +4,49 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * Animales
+ *
+ * @ORM\Table(name="animales")
+ * @ORM\Entity
+ */
 class Animal
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
     private $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tipo", type="string", length=255, nullable=false)
+     */
     private $tipo;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=255, nullable=false)
+     */
     private $color;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="raza", type="string", length=255, nullable=false)
+     */
     private $raza;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255, nullable=false)
+     */
+    private $nombre;
 
     public function getId(): ?int
     {
